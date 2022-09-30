d9b9045b60ffa336b6aa7fb3b255186c3f496423
Root-4 <90972608+Root-4@users.noreply.github.com>
2022-09-30 22:53:29 +0530

    Update main.py

--- cipher/main.py
+++ cipher/main.py
@@ -2,5 +2,17 @@ import sys
 from rot13 import rot13
 from fernet import encryption, decryption
 
-print("Hello Encrypted is :", encryption("Hello"))
-print("Encrypted text in ROT-13 cipher is ", rot13("HellO"))
+def main():
+    choice=int(input("Press 1 for Encryption, 2 for Decryption"))
+    message=input("Enter the message :")
+
+    if(choice==1):
+        print("Encrypted Fernet Text: ", encryption(message))
+        print("Encrypted text in ROT-13 cipher: ", rot13(message))
+    elif(choice==2):
+        print("Decrpted Fernet Text: ", decryption(message))
+    else
+        print("Bad Choice")
+
+if __name__=="__main__":
+    main()
